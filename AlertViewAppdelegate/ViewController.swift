b09630cc77c475a75607e4f491e08202fccce2a1
Munish <manishkpr@gmail.com>
2015-08-26 09:10:46 +0800

    initial commit

---
+++ AlertViewAppdelegate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertViewAppdelegate
+//
+//  Created by Munish on 8/25/15.
+//  Copyright (c) 2015 manishkpr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
